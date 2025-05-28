1337374f53085b7e716aca0622394a1667e52f35
DEVANAND GAUR <96323671+DEVANANDGAUR9@users.noreply.github.com>
2025-05-28 11:38:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
-  default     = "t2.medium"
+  default     = "t2.micro"
 }
 
 variable "min_size" {
